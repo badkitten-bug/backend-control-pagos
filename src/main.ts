b0417c69bf26506791628496899b5b2e4d5ed30f
Steve Gomez <104510289+badkitten-bug@users.noreply.github.com>
2026-02-12 19:07:31 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,8 @@ async function bootstrap() {
     }),
   );
   
-  await app.listen(3001);
-  console.log('🚀 Backend running on http://localhost:3001');
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Backend running on port ${port}`);
 }
 bootstrap();
